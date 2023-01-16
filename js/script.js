966b0181880e96dda14569f4c6e5de69bfbb7534
devsalauddin <mdsalauddinremon@gmail.com>
2023-01-16 08:52:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+ // owl-carousel
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    items:2,
+    loop:true,
+    infinate: true,
+    margin:40,
+    speed: 4000,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+
+        // wow js init
+        new WOW().init();
+        // counterUp-js
+        $('.stat-number').each(function () {
+            var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 5000,
+                step: function (func) {
+                    $(this).text(parseFloat(func).toFixed(size));
+                }
+            });
+        });
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
