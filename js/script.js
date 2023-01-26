e4e4530d5c0933b8a7219753d322511bdc086f39
devsalauddin <mdsalauddinremon@gmail.com>
2023-01-26 13:15:32 +0600

    added testimonial page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+//  brand
+$('.brand').slick({
+    
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  dots: true,
+  infinate: true,
+  pauseOnHover:false,
+  speed: 3000,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+})
+ 
  // owl-carousel
   var owl = $('.owl-carousel');
   owl.owlCarousel({
