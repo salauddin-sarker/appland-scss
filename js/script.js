53d9e660b6ef79d2f5d694d901096576fbef4566
devsalauddin <mdsalauddinremon@gmail.com>
2023-02-03 17:49:09 +0600

    fixed sticky header finally

--- js/script.js
+++ js/script.js
@@ -132,3 +132,7 @@ btn.on('click', function(e) {
 
 
 // sticky-header
+window.addEventListener('scroll',function(){
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 200);
+});
